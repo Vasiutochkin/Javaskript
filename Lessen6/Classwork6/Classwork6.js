4b682bad7a731e6ac5c3b7dbee1dadf4cf4196f3
Vasiutochkin <Vasiutochkin1.gmail.com>
2022-02-14 21:15:12 +0200

    Homework6

---
+++ Lessen6/Classwork6/Classwork6.js
@@ -0,0 +1,71 @@
+// // - Дано список імен.
+// let n1 = 'Harry..Potter'
+// let n2 = 'Ron---Whisley'
+// let n3 = 'Hermione__Granger'
+// // Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
+// // let n1 = 'Harry Potter'
+// // let n2 = 'Ron Whisley'
+// // let n3 = 'Hermione Granger'
+//
+// const a = (arr) =>{
+//     return arr
+//         .replaceAll(".."," ")
+//         .replaceAll("---", " ")
+//         .replaceAll("__"," ")
+//
+// };
+// console.log(a(n1));
+// console.log(a(n2));
+// console.log(a(n3));
+
+//  - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+// const a = (length, nam) => {
+//     let ff = [];
+//     for (let i = 0; i < length; i++) {
+//         ff.push(Math.round(Math.random() * nam))
+//     }
+//     return ff;
+// }
+// let a1 = a(38, 50);
+// console.log(a1);
+//
+// // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
+// // Відсортувати його за допомоги sort
+//
+// // 1 варіант використовуючи попередню функцію
+// a1.sort((a, b) => a - b);
+// console.log(a1)
+
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
+// відфільтрувати  його за допомоги filter, залишивши тільки парні числа
+const a = (length, nam) => {
+    let ff = [];
+    for (let i = 0; i < length; i++) {
+        ff.push(Math.round(Math.random() * nam))
+    }
+    return ff.filter(value => value % 2 ===0);
+}
+let a1 = a(38, 50);
+console.log(a1);
+
+// - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) .
+// за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого,
+// або навпаки в залежності від значення аргументу direction.
+// let nums = [11,21,3];
+// sortNums('ascending') // [3,11,21]
+// sortNums('descending') // [21,11,3]
+//
+//
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// -- відсортувати його за спаданням за monthDuration
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// //
